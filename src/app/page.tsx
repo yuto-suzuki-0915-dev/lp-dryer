1a89ba27d9b2058967c021baeb9613d43ae73811
Yuto Suzuki <suzuyuto0915@gmail.com>
2026-09-07 11:16:43 +0900

    feat: implement solution section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import FirstView from "@/components/sections/FirstView";
 import ProblemSection from "@/components/sections/ProblemSection";
+import SolutionSection from "@/components/sections/SolutionSection";
 
 export default function Home() {
   return (
     <main>
       <FirstView />
       <ProblemSection />
+      <SolutionSection />
     </main>
   );
 }
